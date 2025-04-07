b235e8888c456830f347945d25ad196e061b6539
xogur <snsk656@naver.com>
2025-04-07 19:41:32 +0900

    셀레니움 테스트

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -1,22 +1,26 @@
 from airflow import DAG
-import datetime
 import pendulum
-from airflow.operators.bash import BashOperator
+import datetime
+from airflow.operators.python import PythonOperator
+import random
 
 with DAG(
-    dag_id="dags_bash_operator",
-    schedule="0 0 * * *",
+    dag_id="dags_python_operator",
+    schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-    bash_t1 = BashOperator(
-        task_id="bash_t1",
-        bash_command="echo whoami",
+    def select_fruit():
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
+
+        options = Options()
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        driver = webdriver.Chrome(options=options) 
+
+    py_t1 = PythonOperator(
+        task_id='py_t1',
+        python_callable=select_fruit
     )
 
-    bash_t2 = BashOperator(
-        task_id="bash_t2",
-        bash_command="echo $HOSTNAME",
-    )
-
-    bash_t1 >> bash_t2
+    py_t1
