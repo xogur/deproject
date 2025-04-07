67335914fbddbf342b2bde6e04497994ab87062e
xogur <snsk656@naver.com>
2025-04-07 20:22:02 +0900

    테스트

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -16,8 +16,10 @@ with DAG(
 
         options = Options()
         options.add_argument('--headless')
-        options.add_argument('--window-size=1920,1080')
-        options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3')
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")  # (optional but often helps)
+        options.add_argument("--remote-debugging-port=9222")
         driver = webdriver.Chrome(options=options)
 
     py_t1 = PythonOperator(
