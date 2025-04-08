bb9975dc7a9522af775707452ad787857f29b57c
xogur <snsk656@naver.com>
2025-04-08 17:20:25 +0900

    test

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -15,7 +15,7 @@ with DAG(
         from selenium.webdriver.chrome.options import Options
 
         options = Options()
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")  # (optional but often helps)
