84a6f09fdd5dec22e48d3da4474a0b8734420504
xogur <snsk656@naver.com>
2025-04-11 12:07:40 +0900

    postgres 연결

---
+++ dags/dags_sale_load.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+with DAG(
+        dag_id='dags_sale_load',
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        schedule='0 7 * * *',
+        catchup=False
+) as dag:
+    def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
+        postgres_hook = PostgresHook(postgres_conn_id)
+        postgres_hook.bulk_load(tbl_nm, file_nm)
+
+    insrt_postgres = PythonOperator(
+        task_id='insrt_postgres',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id': 'deproject_sale_info',
+                   'tbl_nm':'deproject',
+                   'file_nm':'/opt/airflow/files/musinsa_products.csv'}
+    )
