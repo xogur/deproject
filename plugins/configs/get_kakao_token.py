30f479bdef6b75136902943b7b7001caadf2b72d
xogur <snsk656@naver.com>
2025-04-11 16:39:42 +0900

    카카오톡 연동

--- plugins/configs/get_kakao_token.py
+++ plugins/configs/get_kakao_token.py
@@ -1,9 +1,10 @@
 import requests
+from airflow.models import Variable
 # client_id, authorize_code 노출 주의, 실제 값은 임시로만 넣고 Git에 올라가지 않도록 유의
 
-client_id = 'ee58d842aef0ec66fd27057419d5ecf0'
+client_id = Variable.get("kakao_client_secret")
 redirect_uri = 'https://example.com/oauth'
-authorize_code = 'T-nXSAgPLKKGK7xapWWgFq8WTF0HGBJ-Pv6JgmwocLboZo-7uYJWeQAAAAQKFxZiAAABliO0FmXC3p98Pd5TpQ'
+authorize_code = Variable.get("kakao_tokens")
 
 
 token_url = 'https://kauth.kakao.com/oauth/token'
